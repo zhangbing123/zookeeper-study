a64b4584d43083d43507a7d774f107db4ec81c5b
jm005807 <bing.zhang@baozun.com>
2020-11-25 13:20:37 +0800

    zookeeper学习

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/version/util/Info.java
@@ -0,0 +1,16 @@
+package org.apache.zookeeper.version.util;
+
+/**
+ * @description:
+ * @author: zhangbing
+ * @create: 2020-11-24 10:31
+ **/
+public interface Info {
+    int MAJOR = 1;
+    int MINOR = 0;
+    int MICRO = 0;
+    String QUALIFIER = null;
+    int REVISION = -1;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2020-10-15";
+}
